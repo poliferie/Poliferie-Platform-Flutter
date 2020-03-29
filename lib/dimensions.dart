c0e34b7b336d70f0b0a938fb12476b4ff27cabc9
Andrea Merlo <merlo.andrea94@gmail.com>
2020-03-29 20:50:11 +0200

    lib/dimensions.dart: Update docs for dimensions.dart

--- lib/dimensions.dart
+++ lib/dimensions.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/widgets.dart';
 
+/// Class which holds layout parameters for the entire app.
 class AppDimensions {
+  // Paddings
   static const EdgeInsets bodyPadding =
       EdgeInsets.fromLTRB(30.0, 50.0, 30.0, 20.0);
   static const EdgeInsets subHeadlinePadding =
       EdgeInsets.fromLTRB(0, 20.0, 0, 20.0);
-  static const double bottomNavigationBarIconSize = 32.0;
   static const EdgeInsets searchBodyPadding =
       EdgeInsets.fromLTRB(30.0, 20.0, 30.0, 20.0);
+
+  // Sizes
+  static const double bottomNavigationBarIconSize = 32.0;
 }
