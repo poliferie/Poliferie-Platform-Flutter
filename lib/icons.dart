7137a044323691ceecb220b7d129a25c575a9f47
Andrea Merlo <merlo.andrea94@gmail.com>
2020-03-29 20:25:29 +0200

    lib/icons.dart: Update docs for icons.dart

--- lib/icons.dart
+++ lib/icons.dart
@@ -1,12 +1,18 @@
 import 'package:flutter/widgets.dart';
 import 'package:flutter/material.dart';
 
+/// Class which holds icons for the entire app.
 class AppIcons {
+  // Navigation Bar
   static const IconData home = IconData(59530, fontFamily: 'MaterialIcons');
-  static const IconData compare = IconData(58297, fontFamily: 'MaterialIcons');
   static const IconData search = IconData(59574, fontFamily: 'MaterialIcons');
+  static const IconData compare = IconData(58297, fontFamily: 'MaterialIcons');
   static const IconData profile = IconData(59475, fontFamily: 'MaterialIcons');
+
+  // App Bar
   static const IconData settings = IconData(59576, fontFamily: 'MaterialIcons');
+
+  // General
   static const IconData university =
       IconData(59471, fontFamily: 'MaterialIcons');
   static const IconData course = IconData(57391, fontFamily: 'MaterialIcons');
