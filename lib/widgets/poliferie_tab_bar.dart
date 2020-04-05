45edcee7e147c4743e973eead3f60945b086b273
Andrea Merlo <merlo.andrea94@gmail.com>
2020-04-05 20:06:01 +0200

    lib/widgets/poliferie_tab_bar.dart: Update TabBar label color and indicator

--- lib/widgets/poliferie_tab_bar.dart
+++ lib/widgets/poliferie_tab_bar.dart
@@ -6,6 +6,8 @@ import 'package:Poliferie.io/strings.dart';
 class PoliferieTabBar extends StatelessWidget implements PreferredSizeWidget {
   const PoliferieTabBar({Key key}) : super(key: key);
 
+  final double _borderRadius = 10.0;
+
   @override
   Size get preferredSize {
     return Size.fromHeight(AppBar().preferredSize.height);
@@ -15,15 +17,18 @@ class PoliferieTabBar extends StatelessWidget implements PreferredSizeWidget {
   @override
   Widget build(BuildContext context) {
     return TabBar(
-      labelColor: Styles.poliferieRed,
-      unselectedLabelColor: Styles.poliferieWhite,
+      labelColor: Styles.poliferieWhite,
+      unselectedLabelColor: Styles.poliferieRed,
       indicatorSize: TabBarIndicatorSize.label,
       indicator: BoxDecoration(
-          borderRadius: BorderRadius.only(
-            topLeft: Radius.circular(10),
-            topRight: Radius.circular(10),
-          ),
-          color: Colors.white),
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(_borderRadius),
+          topRight: Radius.circular(_borderRadius),
+          bottomLeft: Radius.circular(_borderRadius),
+          bottomRight: Radius.circular(_borderRadius),
+        ),
+        color: Styles.poliferieRed,
+      ),
       tabs: [
         Tab(
           child: Align(
