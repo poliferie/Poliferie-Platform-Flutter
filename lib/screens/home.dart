687fc15dfa40b9a0e63b8794d93a493103b6dddc
Andrea Merlo <merlo.andrea94@gmail.com>
2020-06-06 18:20:33 +0200

    lib: Prepare home card to accept the onTap field

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -4,15 +4,16 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'package:Poliferie.io/bloc/card.dart';
 import 'package:Poliferie.io/repositories/repositories.dart';
-import 'package:Poliferie.io/dimensions.dart';
 import 'package:Poliferie.io/models/card.dart';
+import 'package:Poliferie.io/models/article.dart';
+import 'package:Poliferie.io/screens/base.dart';
+
+import 'package:Poliferie.io/dimensions.dart';
 import 'package:Poliferie.io/strings.dart';
 import 'package:Poliferie.io/styles.dart';
 
 import 'package:Poliferie.io/widgets/poliferie_app_bar.dart';
 import 'package:Poliferie.io/widgets/poliferie_card.dart';
-
-import 'package:Poliferie.io/models/article.dart';
 import 'package:Poliferie.io/widgets/poliferie_article.dart';
 
 // TODO(@amerlo): Where the repository have to be declared?
@@ -61,25 +62,41 @@ class HomeScreenBody extends StatelessWidget {
     );
   }
 
+  // TODO(@amerlo): How to access HomeScreen and change selected screen?
   Widget _buildRowCards() {
     return Container(
-        child: Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: <Widget>[
-        PoliferieCard(_coursesCard),
-        PoliferieCard(_universitiesCard),
-      ],
-    ));
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          PoliferieCard(
+            _coursesCard,
+            onTap: () {},
+          ),
+          PoliferieCard(
+            _universitiesCard,
+            onTap: () {},
+          ),
+        ],
+      ),
+    );
   }
 
   Widget _buildBody(BuildContext context, List<CardInfo> cards) {
-    final _cards = cards.asMap().map((index, card) {
-      return MapEntry(index, GestureDetector(
-        onTap: _tryPoliferieArticle(context, which: index),
-        child: PoliferieCard(card),
-      ));
-    }).values.toList();
-    final double bottomPadding = MediaQuery.of(context).padding.bottom + AppDimensions.bodyPadding.bottom;
+    final _cards = cards
+        .asMap()
+        .map((index, card) {
+          return MapEntry(
+            index,
+            GestureDetector(
+              onTap: _tryPoliferieArticle(context, which: index),
+              child: PoliferieCard(card),
+            ),
+          );
+        })
+        .values
+        .toList();
+    final double bottomPadding = MediaQuery.of(context).padding.bottom +
+        AppDimensions.bodyPadding.bottom;
     return ListView(
       padding: AppDimensions.bodyPadding.copyWith(bottom: bottomPadding),
       scrollDirection: Axis.vertical,
@@ -169,6 +186,15 @@ as
 d
 as""";
   Article article1 = Article(id: 1, title: "Titolo", bodyMarkdownSource: md);
-  Article article2 = Article(id: 2, title: "Titolo 2 un pò più lungo dai", subtitle: "Anche col sottotitolo e l'immagine", image: NetworkImage("https://raw.githubusercontent.com/flutter-rus/flutter-rus.github.io/master/images/logo.png", scale: 1.0), bodyMarkdownSource: md);
-  return which == 1 ? PoliferieArticle(article: article1).bottomSheetCaller(context) : PoliferieArticle(article: article2).bottomSheetCaller(context);
+  Article article2 = Article(
+      id: 2,
+      title: "Titolo 2 un pò più lungo dai",
+      subtitle: "Anche col sottotitolo e l'immagine",
+      image: NetworkImage(
+          "https://raw.githubusercontent.com/flutter-rus/flutter-rus.github.io/master/images/logo.png",
+          scale: 1.0),
+      bodyMarkdownSource: md);
+  return which == 1
+      ? PoliferieArticle(article: article1).bottomSheetCaller(context)
+      : PoliferieArticle(article: article2).bottomSheetCaller(context);
 }
