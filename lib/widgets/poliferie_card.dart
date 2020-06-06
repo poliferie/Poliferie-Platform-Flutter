98b15fe5ac378391b7fc94f48e249e33878c4cda
Andrea Merlo <merlo.andrea94@gmail.com>
2020-06-06 18:20:15 +0200

    lib: Add onTap field

--- lib/widgets/poliferie_card.dart
+++ lib/widgets/poliferie_card.dart
@@ -5,32 +5,36 @@ import 'package:Poliferie.io/models/card.dart';
 
 class PoliferieCard extends StatelessWidget {
   final CardInfo card;
+  final Function onTap;
 
-  const PoliferieCard(@required this.card);
+  const PoliferieCard(this.card, {this.onTap});
 
   Widget _buildCard(BuildContext context) {
     final _height = MediaQuery.of(context).size.width * 0.55;
-    return Container(
-      height: _height,
-      child: Card(
-        elevation: 8.0,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(15.0),
-        ),
-        child: Padding(
-          padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 5.0),
-          child: Column(
-            children: <Widget>[
-              Image.asset(
-                card.imagePath,
-                fit: BoxFit.cover,
-                height: _height * 0.50,
-              ),
-              Padding(
-                padding: EdgeInsets.only(top: 20.0),
-                child: Text(card.shortName, style: Styles.cardHead),
-              ),
-            ],
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        height: _height,
+        child: Card(
+          elevation: 8.0,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(15.0),
+          ),
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 5.0),
+            child: Column(
+              children: <Widget>[
+                Image.asset(
+                  card.imagePath,
+                  fit: BoxFit.cover,
+                  height: _height * 0.50,
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 20.0),
+                  child: Text(card.shortName, style: Styles.cardHead),
+                ),
+              ],
+            ),
           ),
         ),
       ),
