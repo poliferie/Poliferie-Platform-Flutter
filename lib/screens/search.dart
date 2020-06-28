1e5b219dd49826e2be37eb6564919a277f84c425
Andrea Merlo <merlo.andrea94@gmail.com>
2020-06-28 17:29:02 +0200

    lib: Move Poliferie Search Delegate to seperate file

--- lib/screens/search.dart
+++ lib/screens/search.dart
@@ -1,7 +1,4 @@
-import 'dart:convert';
-
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'package:Poliferie.io/styles.dart';
@@ -15,103 +12,12 @@ import 'package:Poliferie.io/models/filter.dart';
 import 'package:Poliferie.io/models/item.dart';
 import 'package:Poliferie.io/repositories/search_repository.dart';
 import 'package:Poliferie.io/repositories/filter_repository.dart';
-import 'package:Poliferie.io/screens/item.dart';
-import 'package:Poliferie.io/screens/results.dart';
 
 import 'package:Poliferie.io/widgets/poliferie_filter.dart';
 import 'package:Poliferie.io/widgets/poliferie_app_bar.dart';
 import 'package:Poliferie.io/widgets/poliferie_tab_bar.dart';
 import 'package:Poliferie.io/widgets/poliferie_floating_button.dart';
-
-/// [SearchDelegate] helper class.
-class PoliferieSearchDelegate extends SearchDelegate {
-  final SearchBloc searchBloc;
-
-  PoliferieSearchDelegate({this.searchBloc});
-
-  @override
-  ThemeData appBarTheme(BuildContext context) {
-    assert(context != null);
-    final ThemeData theme = Theme.of(context);
-    assert(theme != null);
-    return theme;
-  }
-
-  @override
-  List<Widget> buildActions(BuildContext context) {
-    return [
-      IconButton(
-        icon: Icon(Icons.clear),
-        onPressed: () {
-          query = '';
-        },
-      ),
-    ];
-  }
-
-  @override
-  Widget buildLeading(BuildContext context) {
-    return IconButton(
-      icon: Icon(Icons.arrow_back),
-      onPressed: () {
-        close(context, null);
-      },
-    );
-  }
-
-  @override
-  Widget buildResults(BuildContext context) {
-    // TODO(@ferrarodav): Here we should include the whole search state and
-    //                provide it to the results screen
-    return ResultsScreen(query);
-  }
-
-  // TODO(@amerlo): Add the feature to highlight with bold the search text
-  //                in the suggestion name
-  @override
-  Widget buildSuggestions(BuildContext context) {
-    searchBloc.add(FetchSuggestions(searchText: query));
-
-    return BlocBuilder<SearchBloc, SearchState>(
-      bloc: searchBloc,
-      builder: (BuildContext context, SearchState state) {
-        if (state is SearchStateLoading) {
-          return CircularProgressIndicator();
-        }
-        if (state is SuggestionStateSuccess) {
-          Widget list = Container(
-            color: Styles.poliferieWhite,
-            child: ListView.builder(
-              itemCount: state.suggestions.length,
-              itemBuilder: (context, index) {
-                var item = state.suggestions[index];
-                return ListTile(
-                  leading: Icon(
-                      item.isCourse() ? AppIcons.course : AppIcons.university),
-                  onTap: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => ItemScreen(item.id),
-                      ),
-                    );
-                  },
-                  title: Text(item.shortName),
-                );
-              },
-            ),
-          );
-          searchBloc.close();
-          return list;
-        }
-        if (state is SearchStateError) {
-          return Text(state.error);
-        }
-        return Text('This widget should never be reached');
-      },
-    );
-  }
-}
+import 'package:Poliferie.io/widgets/poliferie_search_delegate.dart';
 
 class SearchScreen extends StatefulWidget {
   SearchScreen({Key key}) : super(key: key);
