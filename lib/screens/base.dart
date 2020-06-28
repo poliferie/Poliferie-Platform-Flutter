6d18976f66da70247acd5664364cc86f7f643157
Andrea Merlo <merlo.andrea94@gmail.com>
2020-06-28 13:44:42 +0200

    lib: Return to first tab on device back button

--- lib/screens/base.dart
+++ lib/screens/base.dart
@@ -6,22 +6,23 @@ import 'package:Poliferie.io/dimensions.dart';
 
 import 'package:Poliferie.io/screens/screens.dart';
 
-
 class BaseScreen extends StatefulWidget {
   @override
   BaseScreenState createState() => new BaseScreenState();
 }
 
 class BaseScreenState extends State<BaseScreen> {
-  int _selectedScreenIndex = 0;
+  int _selectedIndex = 0;
 
-  // TODO(@amerlo): This has to be used from outside this class
-  void changeScreen(int screen) {
-    if (screen != _selectedScreenIndex && screen < pages.length) {
-      setState(() {
-        _selectedScreenIndex = screen;
-      });
-    }
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  Future<bool> _onWillPop() {
+    setState(() {
+      _selectedIndex = 0;
+    });
   }
 
   // TODO(@amerlo): Is there a more elegant solution
@@ -42,65 +43,68 @@ class BaseScreenState extends State<BaseScreen> {
   /// Build the [BottomNavigationBar] which handles the screens.
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      body: pages[_selectedScreenIndex],
-      extendBody: true,
-      bottomNavigationBar: Container(
-        decoration: BoxDecoration(
-          borderRadius: navigationBarRadius,
-          boxShadow: <BoxShadow>[
-            BoxShadow(
-                color: Colors.black.withOpacity(0.4),
-                spreadRadius: 0,
-                blurRadius: 10),
-          ],
-        ),
-        child: ClipRRect(
-          borderRadius: navigationBarRadius,
-          child: BottomNavigationBar(
-            items: <BottomNavigationBarItem>[
-              BottomNavigationBarItem(
-                title: Text(""),
-                icon: Icon(
-                  AppIcons.home,
-                  size: AppDimensions.bottomNavigationBarIconSize,
-                ),
-              ),
-              BottomNavigationBarItem(
-                title: Text(""),
-                icon: Icon(
-                  AppIcons.search,
-                  size: AppDimensions.bottomNavigationBarIconSize,
-                ),
-              ),
-              BottomNavigationBarItem(
-                title: Text(""),
-                icon: Icon(
-                  AppIcons.compare,
-                  size: AppDimensions.bottomNavigationBarIconSize,
-                ),
-              ),
-              BottomNavigationBarItem(
-                title: Text(""),
-                icon: Icon(
-                  AppIcons.profile,
-                  size: AppDimensions.bottomNavigationBarIconSize,
-                ),
-              ),
+    return WillPopScope(
+      onWillPop: _onWillPop,
+      child: Scaffold(
+        backgroundColor: Colors.white,
+        body: pages[_selectedIndex],
+        extendBody: true,
+        bottomNavigationBar: Container(
+          decoration: BoxDecoration(
+            borderRadius: navigationBarRadius,
+            boxShadow: <BoxShadow>[
+              BoxShadow(
+                  color: Colors.black.withOpacity(0.4),
+                  spreadRadius: 0,
+                  blurRadius: 10),
             ],
-            onTap: (index) {
-              setState(() {
-                _selectedScreenIndex = index;
-              });
-            },
-            type: BottomNavigationBarType.fixed,
-            selectedFontSize: 0.0,
-            unselectedFontSize: 0.0,
-            backgroundColor: Styles.poliferieWhite,
-            selectedItemColor: Styles.poliferieRed,
-            unselectedItemColor: Styles.poliferieDarkGrey,
-            currentIndex: _selectedScreenIndex,
+          ),
+          child: ClipRRect(
+            borderRadius: navigationBarRadius,
+            child: BottomNavigationBar(
+              items: <BottomNavigationBarItem>[
+                BottomNavigationBarItem(
+                  title: Text(""),
+                  icon: Icon(
+                    AppIcons.home,
+                    size: AppDimensions.bottomNavigationBarIconSize,
+                  ),
+                ),
+                BottomNavigationBarItem(
+                  title: Text(""),
+                  icon: Icon(
+                    AppIcons.search,
+                    size: AppDimensions.bottomNavigationBarIconSize,
+                  ),
+                ),
+                BottomNavigationBarItem(
+                  title: Text(""),
+                  icon: Icon(
+                    AppIcons.compare,
+                    size: AppDimensions.bottomNavigationBarIconSize,
+                  ),
+                ),
+                BottomNavigationBarItem(
+                  title: Text(""),
+                  icon: Icon(
+                    AppIcons.profile,
+                    size: AppDimensions.bottomNavigationBarIconSize,
+                  ),
+                ),
+              ],
+              onTap: (index) {
+                setState(() {
+                  _selectedIndex = index;
+                });
+              },
+              type: BottomNavigationBarType.fixed,
+              selectedFontSize: 0.0,
+              unselectedFontSize: 0.0,
+              backgroundColor: Styles.poliferieWhite,
+              selectedItemColor: Styles.poliferieRed,
+              unselectedItemColor: Styles.poliferieDarkGrey,
+              currentIndex: _selectedIndex,
+            ),
           ),
         ),
       ),
