267fda6f3fee2f822791cc9ea784638549d22a5d
Andrea Merlo <merlo.andrea94@gmail.com>
2020-09-02 22:06:26 +0200

    Use semantic labels to access icons
    By using semantic labels to access icons we should be safe in case
    of icon id changes.

--- lib/icons.dart
+++ lib/icons.dart
@@ -4,16 +4,15 @@ import 'package:flutter/material.dart';
 /// Class which holds icons for the entire app.
 class AppIcons {
   // Navigation Bar
-  static const IconData home = IconData(59530, fontFamily: 'MaterialIcons');
-  static const IconData search = IconData(59574, fontFamily: 'MaterialIcons');
-  static const IconData compare = IconData(58297, fontFamily: 'MaterialIcons');
-  static const IconData profile = IconData(59475, fontFamily: 'MaterialIcons');
+  static const IconData home = Icons.home;
+  static const IconData search = Icons.search;
+  static const IconData compare = Icons.compare;
+  static const IconData profile = Icons.supervised_user_circle;
 
   // App Bar
-  static const IconData settings = IconData(59576, fontFamily: 'MaterialIcons');
+  static const IconData settings = Icons.settings;
 
   // General
-  static const IconData university =
-      IconData(59471, fontFamily: 'MaterialIcons');
-  static const IconData course = IconData(57391, fontFamily: 'MaterialIcons');
+  static const IconData university = Icons.school;
+  static const IconData course = Icons.book;
 }
